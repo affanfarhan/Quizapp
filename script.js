17613885251ee509a91ffe6c59203a306f2702f6
affanfarhan <affanfarhanfarhaniqbal@gmail.com>
2024-12-19 15:42:17 +0500

    First commiit

---
+++ script.js
@@ -0,0 +1,58 @@
+let allQuestion  * [
+
+    {
+
+        question : "Cpu stand for?",
+        Option : [
+         {answer:"Central Processing Unit",iscorrect:true},
+         {answer:"Central Process Unit",iscorrect:false},
+         {answer:"Central programming Unit",iscorrect:false},
+         {answer:"Central Processor Unit",iscorrect:false}
+        ]
+    },
+
+    {
+        question : "Which of the following is the correct way to declare a variable in JavaScript?",
+        Option : [
+            {answer:"var=myvariable;",iscorrect:false},
+            {answer:"variable myvariable;",iscorrect:false},
+            {answer:"let myvariable;",iscorrect:true},
+            {answer:"declare myvariable;",iscorrect:false}
+        ]
+    },
+
+    {
+        question : "What will the following code output?",
+        Option : [
+            {answer:"null",iscorrect:false},
+            {answer:"string",iscorrect:false},
+            {answer:"undefined",iscorrect:false},
+            {answer:"object",iscorrect:true}
+        ]
+    },
+
+    {
+        question : "How can you create a function in JavaScript?",
+        Option : [
+            {answer:"func my function() {}",iscorrect:true},
+            {answer:"function: my function() {}",iscorrect:false},
+            {answer:"create function my function() {}",iscorrect:false},
+            {answer:"function my function() {}",iscorrect:false}
+                
+        ]
+    },
+
+
+    {
+        question : "Which of the following is used to find the length of an array in JavaScript?",
+        Option : [
+            {answer:"array.getlength()",iscorrect:false},
+            {answer:"array.length()",iscorrect:true},
+            {answer:"array.size()",iscorrect:false},
+            {answer:"array.length()",iscorrect:true}
+        ]
+    },
+
+
+]
+console.log(allQuestion);
